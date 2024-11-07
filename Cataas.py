7bd595dbc3439ef3067934f04c7a209168a030cd
Аня <anna.login08@gmail.com>
2024-11-08 01:29:49 +0200

    Добавлена кнопка для загрузки нового изображения

--- Cataas.py
+++ Cataas.py
@@ -4,9 +4,10 @@ import requests
 from io import BytesIO
 
 from bottle import response
+from pygame.display import update
 
 
-def load_image():
+def load_image(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -17,20 +18,25 @@ def load_image():
         print(f"Произошла ошибка: {e}")
         return None
 
+def set_image():
+    img = load_image(url)
+    if img:
+        label.config(image = img)
+        label.image = img
 
 
 window = Tk()
 window.title("Cats!")
-window.geometry("600x400")
+window.geometry("700x500")
 
 label = Label()
 label.pack()
 
-url = "https://cataas.com/cat"
-img = load_image(url)
+update_button = Button(text="Следующий котик", command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+url = "https://cataas.com/cat"
+
+set_image()
 
 window.mainloop()
