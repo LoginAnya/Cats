b0071e839d7fffa9e81bb4301a20527bd393cb9a
Аня <anna.login08@gmail.com>
2024-11-08 01:51:54 +0200

    Каждое изображение открывается в новом окне

--- Cataas.py
+++ Cataas.py
@@ -19,12 +19,15 @@ def load_image(url):
         print(f"Произошла ошибка: {e}")
         return None
 
-def set_image():
+def open_new_window():
     img = load_image(url)
     if img:
-        label.config(image = img)
+        new_window = Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x480")
+        label = Label(new_window, image = img)
+        label.pack()
         label.image = img
-
 
 
 def exit():
@@ -35,20 +38,16 @@ window = Tk()
 window.title("Cats!")
 window.geometry("700x520")
 
-label = Label()
-label.pack()
 
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
 file_menu = Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Файл", menu=file_menu)
-file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
 file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
-
-set_image()
 
 window.mainloop()
