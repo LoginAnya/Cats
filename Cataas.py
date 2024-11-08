c3ec23bbdf6a1f54205b60a4e93da838306680e5
Аня <anna.login08@gmail.com>
2024-11-08 02:17:17 +0200

    Добавлен выпадающий список для выбора тегов. Поле ввода удалено

--- Cataas.py
+++ Cataas.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from PIL import Image, ImageTk
 import requests
 from io import BytesIO
@@ -6,6 +7,7 @@ from io import BytesIO
 from bottle import response
 from pygame.display import update
 
+Allowed_tags = ["sleep", "jump", "fight", "black", "white", "cute", "little"]
 
 def load_image(url):
     try:
@@ -20,7 +22,7 @@ def load_image(url):
         return None
 
 def open_new_window():
-    tag = tag_entry.get()
+    tag = tag_combobox.get()
     url_tag = f"https://cataas.com/cat/{tag}" if tag else "https://cataas.com/cat"
     img = load_image(url_tag)
 
@@ -41,12 +43,6 @@ window = Tk()
 window.title("Cats!")
 window.geometry("700x520")
 
-tag_entry = Entry()
-tag_entry.pack()
-
-load_button = Button(text="Загрузить по тегу", command=open_new_window)
-load_button.pack()
-
 menu_bar = Menu(window)
 window.config(menu=menu_bar)
 
@@ -57,5 +53,14 @@ file_menu.add_separator()
 file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
+
+tag_label = Label(text="Выбери тег")
+tag_label.pack()
+
+tag_combobox = ttk.Combobox(values=Allowed_tags)
+tag_combobox.pack()
+
+load_button = Button(text="Загрузить по тегу", command=open_new_window)
+load_button.pack()
 
 window.mainloop()
